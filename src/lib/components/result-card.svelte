4920e944f11b933384eba2870f1daa2448714cf9
iigorfelipe <iigorfelipe@gmail.com>
2025-06-29 23:31:43 -0300

    feat: create result card component

---
+++ src/lib/components/result-card.svelte
@@ -0,0 +1,70 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let name;
+	export let age;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('clear');
+	}
+</script>
+
+<div class="container">
+	<div class="result-card">
+		<p class="result-card-name">{name}</p>
+		<p class="result-card-age">{age} anos</p>
+	</div>
+	<button on:click={handleClick}>Tentar com outro nome</button>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.result-card {
+		text-align: center;
+		padding: 1.5rem;
+		background: linear-gradient(to right, #ecfdf5, #f0fdfa);
+		border-radius: 0.75rem;
+		border: 1px solid #a7f3d0;
+		gap: 0.5rem;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.result-card-name {
+		color: #1f2937;
+		font-size: 1.5rem;
+		font-weight: 700;
+	}
+
+	.result-card-age {
+		font-size: 2.25rem;
+		font-weight: 700;
+		background: linear-gradient(to right, #10b981, #0d9488);
+		background-clip: text;
+		color: transparent;
+	}
+
+	button {
+		border-radius: 0.375rem;
+		font-size: 0.875rem;
+		height: 2.5rem;
+		padding: 0.5rem 1rem;
+		border: 1px solid #a7f3d0;
+		background-color: transparent;
+		cursor: pointer;
+		transition:
+			background-color 0.2s ease,
+			color 0.2s ease;
+	}
+
+	button:hover {
+		background-color: #ecfdf5;
+	}
+</style>
